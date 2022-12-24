2752dc7cc61a4167ec59d7e203b08ff4e319d66b
Vatsal Harlalka <harlalka.vatsal@gmail.com>
2022-12-24 13:28:53 +0530

    fixing code

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// selecting elements
+let diceEl = document.querySelector('.dice');
+let score1 = document.querySelector('#score--0');
+let score2 = document.getElementById('score--1');
+
+score1.textContent = 0;
+score2.textContent = 0;
+diceEl.classList.add('hidden');
