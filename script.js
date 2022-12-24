f235ff9078b38203b7c8c2a792a56891b0d6251b
Vatsal Harlalka <harlalka.vatsal@gmail.com>
2022-12-24 15:42:45 +0530

    implement hold score

--- script.js
+++ script.js
@@ -4,8 +4,8 @@
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 let diceEl = document.querySelector('.dice');
-let score1 = document.querySelector('#score--0');
-let score2 = document.getElementById('score--1');
+let score0 = document.querySelector('#score--0');
+let score1 = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 const btnNew = document.querySelector('.btn--new');
@@ -15,9 +15,17 @@ let currentScore = 0;
 let activePlayer = 0;
 const scores = [0, 0];
 
+score0.textContent = 0;
 score1.textContent = 0;
-score2.textContent = 0;
 diceEl.classList.add('hidden');
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0; //ternary operator
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -36,10 +44,18 @@ btnRoll.addEventListener('click', function () {
     // current0El.textContent = currentScore;
   } else {
     // switch player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0; //ternary operator
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
-    currentScore = 0;
+    switchPlayer();
   }
+});
+
+btnHold.addEventListener('click', function () {
+  // Add current score to active player's score
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  // check if player's score >= 100
+  // Finish game
+  // Switch to next player
+  switchPlayer();
 });
